a69f9af1d38261b469e8d4557e80de4f2618cfa9
Kevin Schaich <kschaich@palantir.com>
2019-01-24 17:48:24 -0800

    Full transaction list

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet } = require('./lib/update')
 
 ;(async () => {
+  console.log('Fetching Transactions...');
+  
   const transactions = await fetchTransactions()
+
+  if (transactions.length >= 500) {
+    console.error("More than 500 transactions for this month!")
+  }
+  
   const updates = transformTransactionsToUpdates(transactions)
   updateSheet(updates)
 })()
