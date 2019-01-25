06e4e9b9a010269c16123fc057c721e0de006320
Kevin Schaich <kschaich@palantir.com>
2019-01-24 18:36:06 -0800

    Remove yarn.locK

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const _ = require('lodash')
 
 const { fetchTransactions } = require('./lib/fetch')
 const { transformTransactionsToUpdates } = require('./lib/transform')
@@ -7,7 +8,7 @@ const { updateSheet, addSheet, clearSheet } = require('./lib/update')
 ;(async () => {
   console.log('Fetching Transactions...');
   
-  const transactions = await fetchTransactions()
+  const transactions = await _.sortBy(fetchTransactions(), 'date')
 
   if (transactions.length >= 500) {
     console.error("More than 500 transactions for this month!")
