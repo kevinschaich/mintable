facb79b78e0d59f77caab70e4badec52cc568a83
kevinschaich <schaich.kevin@gmail.com>
2019-01-25 14:31:38 -0800

    All functions working

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-require('dotenv').config()
-const _ = require('lodash')
+require("dotenv").config();
+const _ = require("lodash");
 
-const { fetchTransactions } = require('./lib/fetch')
-const { transformTransactionsToUpdates } = require('./lib/transform')
-const { updateSheet, addSheet, clearSheet } = require('./lib/update')
+const { fetchTransactions } = require("./lib/fetch");
+const { transformTransactionsToUpdates } = require("./lib/transform");
+const { updateSheet, addSheet, clearSheet, getSheets } = require("./lib/update");
 
-;(async () => {
-  console.log('Fetching Transactions...');
-  
-  const transactions = await _.sortBy(fetchTransactions(), 'date')
+(async () => {
+  console.log("Fetching Transactions...");
+
+  const transactions = await _.sortBy(fetchTransactions(), "date");
 
   if (transactions.length >= 500) {
-    console.error("More than 500 transactions for this month!")
+    console.error("More than 500 transactions for this month!");
   }
 
-  const updates = transformTransactionsToUpdates(transactions)
-  clearSheet("Sheet1")
-  updateSheet(updates)
+  const sheets = await getSheets();
+  const updates = transformTransactionsToUpdates(transactions);
+  console.log(sheets);
+  clearSheet("Sheet1");
+  updateSheet(updates);
   // addSheet('asdfasdf')
-})()
+})();
