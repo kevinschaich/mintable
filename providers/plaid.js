f1a0125a0cf37f79716c1f9f0a4ec3d0477650ca
Kevin Schaich <schaich.kevin@gmail.com>
2019-02-05 18:00:54 -0800

    Pull account details and use official account names (#14)

--- providers/plaid.js
+++ providers/plaid.js
@@ -1,10 +1,12 @@
 const moment = require('moment');
 const _ = require('lodash');
-const { fetchTransactions } = require('../lib/plaid');
+const { fetchTransactions, fetchBalances } = require('../lib/plaid');
 
 exports.getTransactions = async (transactionColumns, categoryOverrides, currentMonthSheetTitle) => {
-  const sanitizeTransaction = (transaction) => {
+  const sanitizeTransaction = (transaction, accounts) => {
     let sanitized = transaction;
+
+    sanitized['account_details'] = _.get(accounts, transaction.account_id, {});
 
     /*
      * Explode out Plaid's object hierarchy.
@@ -20,19 +22,19 @@ exports.getTransactions = async (transactionColumns, categoryOverrides, currentM
      *
      *    { "category.0": "Food and Drink", "category.1": "Restaurants" }
      */
-    _.forEach(transactionColumns, (column) => {
+    _.forEach(transactionColumns, column => {
       sanitized[column] = _.get(sanitized, column);
     });
 
     // Map TRUE to 'y' and FALSE to nothing (used for Pending column)
-    sanitized = _.mapValues(sanitized, (p) => {
+    sanitized = _.mapValues(sanitized, p => {
       let prop = p === true ? 'y' : p;
       prop = p === false ? '' : p;
       return prop;
     });
 
     // Handle category overrides defined in .env
-    _.forEach(categoryOverrides, (override) => {
+    _.forEach(categoryOverrides, override => {
       if (new RegExp(override.pattern, _.get(override, 'flags', '')).test(sanitized.name)) {
         sanitized['category.0'] = _.get(override, 'category.0', '');
         sanitized['category.1'] = _.get(override, 'category.1', '');
@@ -42,18 +44,23 @@ exports.getTransactions = async (transactionColumns, categoryOverrides, currentM
     return sanitized;
   };
 
+  const accounts = await fetchBalances();
+  const clean_accounts = _.keyBy(_.flatten(_.map(accounts, item => item.accounts)), 'account_id');
+
   const transactions = await fetchTransactions();
   const sorted = _.sortBy(transactions, 'date');
-  const sanitized = _.map(sorted, sanitizeTransaction);
+  const sanitized = _.map(sorted, transaction => sanitizeTransaction(transaction, clean_accounts));
+
   const partitioned = _.partition(
     sanitized,
-    transaction => moment(transaction.date)
-      .startOf('month')
-      .format('YYYY.MM') === currentMonthSheetTitle,
+    transaction =>
+      moment(transaction.date)
+        .startOf('month')
+        .format('YYYY.MM') === currentMonthSheetTitle
   );
 
   return {
     currentMonthTransactions: _.map(partitioned[0], transaction => _.at(transaction, transactionColumns)),
     lastMonthTransactions: _.map(partitioned[1], transaction => _.at(transaction, transactionColumns))
-  }
+  };
 };
