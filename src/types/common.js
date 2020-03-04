45328364a228b77e8ae6d4f4def61f848b7ffd4f
Kevin Schaich <kschaich@palantir.com>
2020-03-04 12:35:02 -0500

    Accidentally committed build files

--- src/types/common.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-exports.__esModule = true
-var PropertyType
-;(function(PropertyType) {
-    PropertyType['Automated'] = 'automated'
-    PropertyType['Manual'] = 'manual'
-})(PropertyType || (PropertyType = {}))
--- src/types/integrations/csv.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var integrations_1 = require("../integrations");
-exports.defaultGoogleConfig = {
-    name: '',
-    id: integrations_1.IntegrationId.CSV,
-    type: integrations_1.IntegrationType.Import,
-    path: ''
-};
