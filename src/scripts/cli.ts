0e6f3808c2ad4d28d3c08c575a311a82cad49e15
kevinschaich <schaich.kevin@gmail.com>
2020-06-23 23:08:35 -0400

    Asynchronous execution of setup commands

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -2,49 +2,51 @@
 
 import chalk from 'chalk'
 
-const logo = [
-    '\n',
-    '          %',
-    '          %%',
-    '         %%%%%',
-    '       %%%%%%%%',
-    '     %%%%%%%%%%',
-    '   %%%%%%%%%%%%',
-    '  %%%% %%%%%%%%',
-    '  %%%  %%%%%%',
-    '  %%   %%%%%%',
-    '   %   %%%',
-    '        %%%',
-    '         %%',
-    '           %',
-    '\n'
-]
+;(async function() {
+    const logo = [
+        '\n',
+        '          %',
+        '          %%',
+        '         %%%%%',
+        '       %%%%%%%%',
+        '     %%%%%%%%%%',
+        '   %%%%%%%%%%%%',
+        '  %%%% %%%%%%%%',
+        '  %%%  %%%%%%',
+        '  %%   %%%%%%',
+        '   %   %%%',
+        '        %%%',
+        '         %%',
+        '           %',
+        '\n'
+    ]
 
-logo.forEach(line => {
-    console.log(chalk.green(line))
-})
+    logo.forEach(line => {
+        console.log(chalk.green(line))
+    })
 
-console.log(' M I N T A B L E\n')
+    console.log(' M I N T A B L E\n')
 
-const commands = {
-    migrate: ['./migrate.js'],
-    fetch: ['./fetch.js'],
-    'plaid-setup': ['../integrations/plaid/setup.js'],
-    'account-setup': ['../integrations/plaid/add.js'],
-    'google-setup': ['../integrations/google/setup.js']
-}
+    const commands = {
+        migrate: ['./migrate.js'],
+        fetch: ['./fetch.js'],
+        'plaid-setup': ['../integrations/plaid/setup.js'],
+        'account-setup': ['../integrations/plaid/add.js'],
+        'google-setup': ['../integrations/google/setup.js']
+    }
 
-const arg = process.argv[2]
+    const arg = process.argv[2]
 
-if (arg == 'setup') {
-    require('../integrations/plaid/setup.js').default()
-    require('../integrations/google/setup.js').default()
-} else if (commands.hasOwnProperty(arg)) {
-    commands[arg].forEach(command => require(command).default())
-} else {
-    console.log('\nusage: mintable <command>\n')
-    console.log("available commands:")
-    Object.keys(commands)
-        .concat(['setup'])
-        .forEach(command => console.log(`\t${command}`))
-}
+    if (arg == 'setup') {
+        await require('../integrations/plaid/setup.js').default()
+        await require('../integrations/google/setup.js').default()
+    } else if (commands.hasOwnProperty(arg)) {
+        commands[arg].forEach(command => require(command).default())
+    } else {
+        console.log('\nusage: mintable <command>\n')
+        console.log('available commands:')
+        Object.keys(commands)
+            .concat(['setup'])
+            .forEach(command => console.log(`\t${command}`))
+    }
+})()
