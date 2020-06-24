c19e6ab9391a20a89e51f842b55a8611c8da6e23
kevinschaich <schaich.kevin@gmail.com>
2020-06-24 11:16:07 -0400

    Pre-Release

--- src/integrations/plaid/add.ts
+++ src/integrations/plaid/add.ts
@@ -13,7 +13,7 @@ export default async () => {
             console.log('\nThis script will help you add accounts to Plaid.\n')
             console.log('\n\t1. A page will open in your browser allowing you to link accounts with Plaid.')
             console.log('\t2. Sign in with your banking provider for each account you wish to link.')
-            console.log("\t3. Click 'Done Linking Accounts' in your browser when you are finished.")
+            console.log("\t3. Click 'Done Linking Accounts' in your browser when you are finished.\n")
 
             const config = getConfig()
             const plaidConfig = config.integrations[IntegrationId.Plaid] as PlaidConfig
