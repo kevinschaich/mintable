7805a37477cdacfede540f27385b817a415b1c7f
David Papp <david.papp@whenthen.com>
2023-01-31 13:32:19 +0100

    🎉 Multi country support for Plaid

--- src/integrations/plaid/plaidIntegration.ts
+++ src/integrations/plaid/plaidIntegration.ts
@@ -117,13 +117,14 @@ export class PlaidIntegration {
 
             app.post('/create_link_token', async (req, res) => {
                 const clientUserId = this.user.client_user_id
+                const country_codes = process.env.COUNTRY_CODES ? process.env.COUNTRY_CODES.split(',') : ['US']
                 const options: CreateLinkTokenOptions = {
                     user: {
                         client_user_id: clientUserId
                     },
                     client_name: 'Mintable',
                     products: ['transactions'],
-                    country_codes: ['US'], // TODO
+                    country_codes, // TODO
                     language: 'en' // TODO
                 }
                 if (req.body.access_token) {
